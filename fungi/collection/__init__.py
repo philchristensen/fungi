864fd49380a80564978c78ff64f334e6246df5bc
Phil Christensen <phil@bubblehouse.org>
2021-12-14 23:23:18 -0500

    fix: move listings to a separate subcommand

--- fungi/collection/__init__.py
+++ fungi/collection/__init__.py
@@ -2,13 +2,10 @@
 """
 Click-specific code to define a CLI.
 """
-import sys
 import json
 import logging
-from typing import List, Any
 
 import click
-import tabulate
 
 from ..core import opensea
 
@@ -28,32 +25,3 @@ def get_collection(slug: str) -> None:
     """
     result = opensea.get_collection(slug)
     click.echo(json.dumps(result))
-
-@collection.command(name="sales")
-@click.argument('slug')
-def list_sales(slug: str) -> None:
-    """
-    List sales in an OpenSea collection.
-    """
-    table: List[Any] = []
-    count = 1
-    page = 0
-    result = {'assets': [True]}
-    try:
-        while(result['assets']):
-            result = opensea.get_collection_assets(slug, offset=page)
-            for asset in result['assets']:
-                if count % 500 == 0:
-                    log.info(f"Scanned {count} records so far, found {len(table)} orders...")
-                if asset['sell_orders'] is not None:  # type: ignore
-                    for order in asset['sell_orders']:  # type: ignore
-                        table.append(dict(
-                            token_id = asset['token_id'],  # type: ignore
-                            created_date = order['created_date'],
-                            base_price = int(order['base_price']) / 10**18
-                        ))
-                count += 1
-                page += 1
-    except KeyboardInterrupt:
-        pass
-    click.echo(tabulate.tabulate(table, headers="keys"))
--- fungi/listings/__init__.py
+++ fungi/listings/__init__.py
@@ -2,11 +2,15 @@
 """
 Click-specific code to define a CLI.
 """
-import json
+import logging
+from typing import List, Any
 
 import click
+import tabulate
 
 from ..core import opensea
+
+log = logging.getLogger(__name__)
 
 @click.group()
 def listings() -> None:
@@ -18,7 +22,27 @@ def listings() -> None:
 @click.argument('slug')
 def get_listings(slug: str) -> None:
     """
-    Get all OpenSea listings for a collection.
+    List sales in an OpenSea collection.
     """
-    # result = opensea.get_collection(slug)
-    # click.echo(json.dumps(result))
+    table: List[Any] = []
+    count = 1
+    page = 0
+    result = {'assets': [True]}
+    try:
+        while(result['assets']):
+            result = opensea.get_collection_assets(slug, offset=page)
+            for asset in result['assets']:
+                if count % 500 == 0:
+                    log.info(f"Scanned {count} records so far, found {len(table)} orders...")
+                if asset['sell_orders'] is not None:  # type: ignore
+                    for order in asset['sell_orders']:  # type: ignore
+                        table.append(dict(
+                            token_id = asset['token_id'],  # type: ignore
+                            created_date = order['created_date'],
+                            base_price = int(order['base_price']) / 10**18
+                        ))
+                count += 1
+                page += 1
+    except KeyboardInterrupt:
+        pass
+    click.echo(tabulate.tabulate(table, headers="keys"))
